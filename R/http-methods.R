c3744bc4cf07cfb503a5a382242640b447e53651
Scott Chamberlain <myrmecocystus@gmail.com>
2016-11-17 12:48:51 -0800

    using fauxpas now for errors, started test suite #1 using fauxpas for errors fix #4 - a bit of hack for now to get proper error class    will have fix in fauxpas later

--- R/http-methods.R
+++ R/http-methods.R
@@ -1,5 +1,29 @@
 nGET <- function(url, query = list(), ...) {
   cli <- crul::HttpClient$new(url = url, opts = list(...))
   res <- cli$get(query = query)
+  errs(res)
   res$parse()
+}
+
+errs <- function(x) {
+  if (x$status_code > 201) {
+    xx <- jsonlite::fromJSON(x$parse())
+    if ("error" %in% names(xx)) {
+      # match by status code
+      fun <- match_err(x$status_code)$new()
+      fun$mssg <- xx$error$detail
+      fun$do_verbose(x)
+    } else {
+      # if no error message in response, just general stop
+      fauxpas::http(x)
+    }
+  }
+}
+
+match_err <- function(code) {
+  tmp <- paste0("fauxpas::",
+                grep("HTTP*", getNamespaceExports("fauxpas"), value = TRUE))
+  fxns <- lapply(tmp, function(x) eval(parse(text = x)))
+  codes <- vapply(fxns, function(z) z$public_fields$status_code, 1)
+  fxns[[which(code == codes)]]
 }
